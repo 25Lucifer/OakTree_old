f7428f7224cdf2f37d429a4fd993495ca4503b0b
Lucifer <zoew1942spe@live.com>
2021-08-02 15:20:39 +0800

    对文章样式进行修改

--- src/main/java/com/tree/blog/util/MarkdownUtils.java
+++ src/main/java/com/tree/blog/util/MarkdownUtils.java
@@ -82,6 +82,11 @@ public class MarkdownUtils {
                 "| 阿什顿发  | 非固定杆 | 撒阿什顿发 |\n" +
                 "\n";
         String a = "[imCoding 爱编程](http://www.lirenmi.cn)";
+        String text = "`B-Tree`通常意味着所有的值都是按顺序存储的,并且每一个叶子叶到跟的距离相同。\n" +
+                "\n" +
+                "![建立在B-Tree结构上的索引](/Users/maple/Desktop/桌面图床/第五章_索引/建立在B-Tree结构上的索引.png)\n" +
+                "\n" +
+                "`B-Tree`索引能够加快访问数据的速度,因为存储引擎不再需要进行全表扫描来获取需要的数据,取而代之的是从索引的根结点开始进行搜索。根结点的槽中存放了指向子节点的指针,存储引擎根据这些指针向下层查找。通过比较结点页的值和要查找的值可以找到合适的指针进入下层结点,这些指针实际上定义了子节点页中值的上限和下限。最终存储引擎要么是找到对应的值,要么该记录不存在。(`B-Tree 的特性`)";
         System.out.println(markdownToHtmlExtensions(a));
     }
 
